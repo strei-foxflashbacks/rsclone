631529bf917c1555c81667db5788641dd6622287
Bator B <batorbb@gmail.com>
2023-02-02 14:33:19 +0100

    feat: add render modal videoplayer and helper createElement

---
+++ src/components/modals/videoplayer/videoPlayer.ts
@@ -0,0 +1,120 @@
+import createElement from '../../../helpers/createElement';
+import './style.scss';
+
+const progressBarRender = () => {
+  const progressBarTime = createElement('progress-bar-time', {
+    class: 'progress-bar-time',
+  });
+  const progressBar = <HTMLInputElement>(
+    createElement('input', { class: 'progress-bar', type: 'range', value: '0' })
+  );
+
+  const time = createElement('div', { class: 'time' });
+  const hour = createElement('span', { class: 'time-hour' }, '-00:');
+  const min = createElement('span', { class: 'time-min' }, '00:');
+  const sec = createElement('span', { class: 'time-sec' }, '00');
+
+  time.append(hour);
+  time.append(min);
+  time.append(sec);
+
+  progressBarTime.append(progressBar);
+  progressBarTime.append(time);
+  return progressBarTime;
+};
+
+const btnControlsRender = (video: HTMLVideoElement) => {
+  const btnControls = createElement('div', { class: 'controls-buttons' });
+
+  const btnControlsLeft = createElement('div', {
+    class: 'controls-buttons-left',
+  });
+  const playPause = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn play-pause' })
+  );
+  playPause.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+      playPause.classList.add('pause');
+    } else {
+      video.pause();
+      playPause.classList.remove('pause');
+    }
+  });
+
+  btnControlsLeft.append(playPause);
+  const skipBackward = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn skip-backward' })
+  );
+  btnControlsLeft.append(skipBackward);
+  const skipForward = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn skip-forward' })
+  );
+  btnControlsLeft.append(skipForward);
+  const volume = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn volume' })
+  );
+  btnControlsLeft.append(volume);
+  btnControls.append(btnControlsLeft);
+
+  const btnControlsRight = createElement('div', {
+    class: 'controls-buttons-right',
+  });
+  const subtitleSoundtrack = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn subtitle-soundtrack' })
+  );
+  btnControlsRight.append(subtitleSoundtrack);
+  const settings = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn settings' })
+  );
+  btnControlsRight.append(settings);
+  const streaming = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn streaming' })
+  );
+  btnControlsRight.append(streaming);
+  const fullscreen = <HTMLButtonElement>(
+    createElement('button', { class: 'controls-btn fullscreen' })
+  );
+  btnControlsRight.append(fullscreen);
+  btnControls.append(btnControlsRight);
+
+  return btnControls;
+};
+
+const videoPlayerRender = (src: string, duration = 0) => {
+  const videoPlayer = createElement('div', { class: 'video-player' });
+  const video = <HTMLVideoElement>(
+    createElement('video', { class: 'video', src: src })
+  );
+  videoPlayer.append(video);
+  console.log(duration);
+
+  video.addEventListener('click', () => {
+    const playPause = document.querySelector('.play-pause');
+    if (video.paused) {
+      video.play();
+      playPause!.classList.add('pause');
+    } else {
+      video.pause();
+      playPause!.classList.remove('pause');
+    }
+  });
+
+  video.addEventListener('ended', () => {
+    const playPause = document.querySelector('.play-pause');
+    playPause?.classList.remove('pause');
+  });
+
+  const close = createElement('div', { class: 'close' });
+  videoPlayer.append(close);
+
+  const controls = createElement('div', { class: 'controls' });
+  controls.append(progressBarRender());
+  controls.append(btnControlsRender(video));
+
+  videoPlayer.append(controls);
+
+  return videoPlayer;
+};
+
+export default videoPlayerRender;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 //styles import
 
 import './style.scss';
+import './components/assets/videos/video.mp4';
+import videoPlayerRender from './components/modals/videoplayer/videoPlayer';
 
 //assets import
 
 // require.context('./assets', false, /\.(?:ico|gif|png|jpg|jpeg|svg|ogg|mp3|wav)$/i);
+const src = './assets/video.mp4';
+document.body.append(videoPlayerRender(src));

5e92093c77040e9a332643c87e78d24077ede710
Inna T <meeoow005@gmail.com>
2023-02-03 18:33:15 +0100

    fix: path to pics

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import './style.scss';
 
 //assets import
 
-require.context('./src/assets', false, /\.(?:ico|gif|png|jpg|jpeg|svg|ogg|mp3|wav)$/i);
+require.context('./components/assets', false, /\.(?:ico|gif|png|jpg|jpeg|svg|ogg|mp3|wav)$/i);
 
 import setCurrentPage from './pages/currentPage';
 import getNews from './templates/main/news';

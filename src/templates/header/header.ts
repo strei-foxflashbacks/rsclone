7389369663129dfe6903998b2d31b4e1eb52458e
Inna T <meeoow005@gmail.com>
2023-02-03 17:06:34 +0100

    feat: create header sections

--- src/templates/header/header.ts
+++ src/templates/header/header.ts
@@ -1,7 +1,10 @@
 import createElement from '../../helpers/createElement';
 
 const getHeader = () :HTMLElement => {
-  const header = createElement('header', { class: 'header-container', id: '1' }, 'header');
+  const header = createElement('header', { class: 'header-container' }, 'header');
+  const logo = createElement('div', { class: 'logo' });
+  const logoImg = createElement('img', { src: '../../components/assets/showjet.png' });
+  header.append(logo, logoImg);
   return header;
 
   // принимать из другой функции компонент кнопки авторизации (чтобы обновлять состояние)

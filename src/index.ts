b51ae4316ec5ea31c9422e070c0d24fa9d63f119
Inna T <meeoow005@gmail.com>
2023-02-03 17:05:26 +0100

    fix: path to pics

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 //styles import
 
 import './style.scss';
-import setCurrentPage from './pages/defaultPage';
-import getNews from './templates/main/news';
-import getMainContent from './templates/main/mainContent';
-setCurrentPage([getNews(), getMainContent()]);
 
 //assets import
 
-// require.context('./assets', false, /\.(?:ico|gif|png|jpg|jpeg|svg|ogg|mp3|wav)$/i);
+require.context('./src/assets', false, /\.(?:ico|gif|png|jpg|jpeg|svg|ogg|mp3|wav)$/i);
+
+import setCurrentPage from './pages/currentPage';
+import getNews from './templates/main/news';
+import getMainContent from './templates/main/mainContent';
+
+
+
+setCurrentPage([getNews(), getMainContent()]);

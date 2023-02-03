f1afb91c8c62f275d4867974cc0b591793911503
Inna T <meeoow005@gmail.com>
2023-02-03 18:32:54 +0100

    feat: create header elements

--- src/templates/header/header.ts
+++ src/templates/header/header.ts
@@ -1,10 +1,26 @@
 import createElement from '../../helpers/createElement';
+import '../../components/assets/showjet.png';
 
 const getHeader = () :HTMLElement => {
-  const header = createElement('header', { class: 'header-container' }, 'header');
-  const logo = createElement('div', { class: 'logo' });
-  const logoImg = createElement('img', { src: '../../components/assets/showjet.png' });
-  header.append(logo, logoImg);
+  const header = createElement('header', { class: 'header-container' } );
+  const container = createElement('div', { class: 'content-container' });
+
+  const logoImg = createElement('img', { src: './assets/showjet.png' });
+
+  const feed = createElement('a', { href: '#' }, 'Лента');
+  const serials = createElement('a', { href: '#' }, 'Сериалы');
+
+  const searchForm = createElement('form', { action: '', method: 'get', class: 'form' });
+  const searchInput = createElement('input', { name: 'search', type: 'search', placeholder: 'Enter...', class: 'form__input' });
+  const searchButton = createElement('button', { type: 'submit', class: 'form__search-button' });
+  const searchIcon = createElement('img', { src: './assets/search.svg', alt: '' });
+  searchForm.append(searchInput, searchButton, searchIcon);
+
+  const signIn = createElement('button', { type: 'button', value: 'Войти', id: 'signInButton' });
+
+  container.append(logoImg, feed, serials, searchForm, signIn);
+
+  header.append(container);
   return header;
 
   // принимать из другой функции компонент кнопки авторизации (чтобы обновлять состояние)

7a905d09beed724776fa6917e82dfa9ec1f5365a
Inna T <meeoow005@gmail.com>
2023-02-04 19:19:36 +0100

    feat: add news styles

--- src/templates/main/news.ts
+++ src/templates/main/news.ts
@@ -3,7 +3,14 @@ import newsData from '../../components/newsData';
 import getNewsItem from './newsItem';
 
 const getNews = (): HTMLElement => {
+  const titleComponent = createElement('div', { class: 'title' });
+  const title = createElement('h1', { class: 'title__text' }, 'Новости');
+  const icon = createElement('img', { class: 'title__icon', src: './assets/news_icon.svg', alt: '' });
+  titleComponent.append(icon, title);
+
   const news = createElement('aside', { class: 'news' });
+  news.append(titleComponent);
+
   newsData.forEach(elem => {
     const newsItem = getNewsItem(elem);
     news.append(newsItem);

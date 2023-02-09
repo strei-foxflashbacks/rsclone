06d7b652718a313643fc985675ff2c0d3e0c5c48
Inna T <meeoow005@gmail.com>
2023-02-09 03:28:32 +0100

    fix: delete unnecessary logic in the func get person

--- src/templates/main/filmPage/person.ts
+++ src/templates/main/filmPage/person.ts
@@ -1,36 +1,21 @@
 import createElement from '../../../helpers/createElement';
 const getPerson = (type: string): HTMLElement => {
-  const container = createElement('div', { class: `${type}` });
-  for (let i = 0; i < 5; i++) {
-    const containerActor = createElement('div', { class: 'person__container' });
-    const person = createElement('div', { class: 'person__item' });
+  const container = createElement('div', { class: `${type} person` });
+  const containerActor = createElement('div', { class: 'person__container' });
+  const person = createElement('div', { class: 'person__item' });
 
-    let portraitUrl = '';
-    let name: string;
-    let filmNameValue: string;
+  const portraitUrl = 'url(\'./assets/person.jpg\')';
+  const name = 'Василий Иванович Петров';
+  const filmNameValue = 'Петр Васильевич Иванов';
 
-    if (type === 'producers') {
-      portraitUrl = 'url(\'./assets/news1.jpg\')';
-      name = 'Rick';
-      filmNameValue = 'Fufel Rick';
-    } else if (type === 'actors') {
-      //portraitUrl = 'url(\'./assets/misicks.webp\')';
-      name = 'mr. Misicks';
-      filmNameValue = 'Misicks';
-    } else {
-      portraitUrl = 'url(\'./assets/news1.jpg\')';
-      name = 'Mr. Poopybutthole';
-      filmNameValue = 'Jschk';
-    }
 
-    person.style.backgroundImage = portraitUrl;
-    const containerNames = createElement('div', { class: 'person__names' });
-    const realName = createElement('div', { class: 'person__real-name' }, name);
-    const filmName = createElement('div', { class: 'person__film-name' }, filmNameValue);
-    containerNames.append(realName, filmName);
-    containerActor.append(person, containerNames);
-    container.append(containerActor);
-  }
+  person.style.backgroundImage = portraitUrl;
+  const containerNames = createElement('div', { class: 'person__names' });
+  const realName = createElement('div', { class: 'person__real-name' }, name);
+  const filmName = createElement('div', { class: 'person__film-name' }, filmNameValue);
+  containerNames.append(realName, filmName);
+  containerActor.append(person, containerNames);
+  container.append(containerActor);
 
   return container;
 };

1d8578083a8efceba6658a57c199380f55ea5fe9
Bator B <batorbb@gmail.com>
2023-02-10 18:29:07 +0100

    feat: add user page link in header

---
+++ src/components/userData.ts
@@ -0,0 +1,6 @@
+import { IUser } from '../types/IUser';
+
+export const user: IUser = {
+  name: 'User123',
+  avatarSrc: '',
+};
--- src/templates/header/header.ts
+++ src/templates/header/header.ts
@@ -1,27 +1,81 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+import { user } from '../../components/userData';
 import createElement from '../../helpers/createElement';
 
-const getHeader = () :HTMLElement => {
-  const header = createElement('header', { class: 'header-container' } );
+const getHeader = (): HTMLElement => {
+  const header = createElement('header', { class: 'header-container' });
   const container = createElement('div', { class: 'content-container' });
 
-  const logoImg = createElement('img', { src: './assets/showjet.png', class: 'logo' });
+  const logoImg = createElement('img', {
+    src: './assets/showjet.png',
+    class: 'logo',
+  });
 
   const containerSections = createElement('div', { class: 'sections' });
-  const feed = createElement('a', { href: '#', class: 'sections__item' }, 'Лента');
-  const serials = createElement('a', { href: '#', class: 'sections__item' }, 'Сериалы');
+  const feed = createElement(
+    'a',
+    { href: '#', class: 'sections__item' },
+    'Лента'
+  );
+  const serials = createElement(
+    'a',
+    { href: '#', class: 'sections__item' },
+    'Сериалы'
+  );
 
-
-  const searchForm = createElement('form', { action: '', method: 'get', class: 'form' });
-  const searchInput = createElement('input', { name: 'search', type: 'search', placeholder: 'Введите поисковой запрос', class: 'form__input' });
-  const searchButton = createElement('button', { type: 'submit', class: 'form__search-button' });
-  const searchIcon = createElement('img', { src: './assets/search.svg', alt: '' });
+  const searchForm = createElement('form', {
+    action: '',
+    method: 'get',
+    class: 'form',
+  });
+  const searchInput = createElement('input', {
+    name: 'search',
+    type: 'search',
+    placeholder: 'Введите поисковой запрос',
+    class: 'form__input',
+  });
+  const searchButton = createElement('button', {
+    type: 'submit',
+    class: 'form__search-button',
+  });
+  const searchIcon = createElement('img', {
+    src: './assets/search.svg',
+    alt: '',
+  });
   searchButton.append(searchIcon);
   searchForm.append(searchInput, searchButton);
 
-  const signIn = createElement('button', { type: 'button', id: 'signInButton', class: 'button' }, 'Войти');
+  const authContainer = createElement('div', {
+    class: 'sections__item sections__auth',
+  });
 
-  containerSections.append(feed, serials, searchForm, signIn);
+  const isAuth = true;
 
+  const userProfilePageBtn = createElement(
+    'a',
+    {
+      class: 'sections__item user-name',
+      href: '#',
+    },
+    `${user.name}`
+  );
+  const userAvatar = createElement('img', {
+    class: 'user-avatar',
+    alt: 'User avatar',
+    src: `${user.avatarSrc || './assets/smallAvatar.svg'}`,
+  });
+  userProfilePageBtn.append(userAvatar);
+
+  if (isAuth) userProfilePageBtn.style.display = 'flex';
+
+  const signIn = createElement(
+    'button',
+    { type: 'button', id: 'signInButton', class: 'button' },
+    `${isAuth ? 'Выйти' : 'Войти'}`
+  );
+  authContainer.append(userProfilePageBtn, signIn);
+
+  containerSections.append(feed, serials, searchForm, authContainer);
 
   container.append(logoImg, containerSections);
 
@@ -29,6 +83,5 @@ const getHeader = () :HTMLElement => {
   return header;
 
   // принимать из другой функции компонент кнопки авторизации (чтобы обновлять состояние)
-
 };
 export default getHeader;

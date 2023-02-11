961a0c27efdc2e69e47de5382b2794fbf05c1ca1
Inna T <meeoow005@gmail.com>
2023-02-11 22:18:25 +0100

    feat: authorization modal styles

--- src/components/modals/authorization/authorization.ts
+++ src/components/modals/authorization/authorization.ts
@@ -4,16 +4,27 @@ import closeModal from '../functions/closeModal';
 const getAuthorizationModal = (): HTMLElement => {
   const container = createElement('div', { class: 'authorization modal' });
   const title = createElement('div', { class: 'authorization__title' }, 'Добро пожаловать!');
-  const text = createElement('p', { class: 'authorization__text' }, 'Введите e-mail и пароль');
-  const form = createElement('form', { action: '#', class: 'authorization__input' });
-  const inputEmail = createElement('input', { type: 'email', placeholder: 'E-mail' });
-  const inputPassword = createElement('input', { type: 'password', placeholder: 'Пароль', class: '' });
+  const cross = createElement('img', { src: './assets/cross.svg', width: '30', height: '30', class: 'close' });
+
+  title.append(cross);
+  const innerContainer = createElement('div', { class: 'authorization__inner-container' });
+  const text = createElement('p', { class: 'authorization__text' }, 'Введите телефон или e-mail и пароль');
+  const form = createElement('form', { action: '#', class: 'authorization__form' });
+
+
+  const inputEmail = createElement('input', { type: 'text', placeholder: 'E-mail', class: 'authorization__input' });
+  const inputPassword = createElement('input', { type: 'password', placeholder: 'Пароль', class: 'authorization__input' });
+  const inputs = createElement('div', { class: 'authorization__inputs' });
+  inputs.append(inputEmail, inputPassword);
+
+  const buttons = createElement('div', { class: 'authorization__buttons' });
   const buttonLogIn = createElement('button', { type: 'submit', class: 'button authorization__button' }, 'Войти');
   const buttonRegister = createElement('button', { type: 'submit', class: 'button authorization__button' }, 'Зарегистрироваться');
+  buttons.append(buttonRegister, buttonLogIn);
 
-  form.append(inputEmail, inputPassword, buttonRegister, buttonLogIn);
-  const cross = createElement('img', { src: './assets/cross.svg', width: '30', height: '30', class: 'close' });
-  container.append(title, text, form, cross);
+  form.append(inputs, buttons);
+  innerContainer.append(text, form);
+  container.append(title, innerContainer);
 
   cross.addEventListener('click', closeModal);
 

6faf27f9de6282f20f4fa4b298cbf4efb8fe804f
Inna T <meeoow005@gmail.com>
2023-02-12 04:54:17 +0100

    refactor: delete unused element

--- src/templates/footer/themeButton.ts
+++
@@ -1,9 +0,0 @@
-import createElement from '../../helpers/createElement';
-
-const getThemeButton = (): HTMLElement => {
-  const container = createElement('div', { class: 'theme-element' });
-  const circle = createElement('div', { class: 'theme-element__circle' });
-  container.append(circle);
-  return container;
-};
-export default getThemeButton;

30a339ef5f098cad2132933c23e632ef970417c5
Inna T <meeoow005@gmail.com>
2023-02-14 18:11:28 +0100

    feat: create variables for change theme

--- src/components/themes/functions/setThemeStyles.ts
+++ src/components/themes/functions/setThemeStyles.ts
@@ -1,6 +1,15 @@
 
 const setThemeStyles = (): void => {
   const body = document.querySelector('body');
+  // const header = document.querySelector('.header-container');
+  // const buttonBack = document.querySelector('.season__back-arrow');
+  // const textarea = document.querySelector('textarea');
+  // const inputs = document.querySelectorAll('input');
+  // const footer = document.querySelector('.footer-container');
+  // const tHeads = document.querySelectorAll('.film-page__th');
+  // const tRows = document.querySelectorAll('.film-page__tr');
+  // const addingButtons = document.querySelectorAll('.adding-button');
+  // const authorization = document.querySelector('.authorization__inner-container');
   if (!body) {
     throw new Error('body is not found');
   }
@@ -18,13 +27,9 @@ const setThemeStyles = (): void => {
   const newsText = document.querySelectorAll('.subtitle');
   newsText.forEach(newsItem => {
     const item = newsItem as HTMLElement;
-    console.log(item);
     item.classList.add('dark-theme');
   });
 
-  // const addDarkThemeStyle = (element: HTMLElement) => {
-  //
-  // };
 
 };
 export default setThemeStyles;

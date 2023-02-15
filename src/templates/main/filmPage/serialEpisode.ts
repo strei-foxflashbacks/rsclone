4d652b5a08e58a37c83486bc1962b91e827c2bbb
Inna T <meeoow005@gmail.com>
2023-02-15 03:36:36 +0100

    refactor: commas

--- src/templates/main/filmPage/serialEpisode.ts
+++ src/templates/main/filmPage/serialEpisode.ts
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/comma-dangle */
 import createElement from '../../../helpers/createElement';
 
 const getSerialEpisode = (
   order: number,
   name: string,
-  rate: boolean
+  rate: boolean,
 ): HTMLElement => {
   const container = createElement('div', { class: 'episode' });
   container.style.backgroundImage = "url('./assets/poster.jpg')";
@@ -15,13 +14,13 @@ const getSerialEpisode = (
     const orderElem = createElement(
       'div',
       { class: 'episode__order' },
-      `серия ${order}`
+      `серия ${order}`,
     );
     const rateValue = rate ? 'бесплатно' : 'по подписке';
     const rateElem = createElement(
       'div',
       { class: 'episode__rate' },
-      rateValue
+      rateValue,
     );
     orderAndRate.append(orderElem, rateElem);
   }

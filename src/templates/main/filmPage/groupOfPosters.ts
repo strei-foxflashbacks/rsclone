30494c4d92d5356be5bc905dfbca0b5ad3630c3f
Inna T <meeoow005@gmail.com>
2023-02-20 16:09:25 +0100

    feat: add styles and handler for trailer

--- src/templates/main/filmPage/groupOfPosters.ts
+++ src/templates/main/filmPage/groupOfPosters.ts
@@ -1,5 +1,6 @@
 import createElement from '../../../helpers/createElement';
 import { Film } from '../../../types/Film';
+import { openVideoPlayer } from '../../../helpers/openVideoPlayer';
 
 const getGroupOfPosters = (film: Film): HTMLElement => {
   const materials = createElement('div', { class: 'materials' });
@@ -18,6 +19,14 @@ const getGroupOfPosters = (film: Film): HTMLElement => {
 
   const bigPoster = createElement('div', { class: 'materials__big' });
   bigPoster.style.backgroundImage = `url(${film.thumbnails[5]})`;
+
+  const playIcon = createElement('img', {
+    class: 'episode__play',
+    src: '/assets/playTrailer.svg',
+  });
+  const trailerLabel = createElement('span', { class: 'episode__rate trailer__label' }, 'Трейлер');
+  playIcon.addEventListener('click', openVideoPlayer);
+  trailer.append(trailerLabel, playIcon);
 
   materials.append(trailer, middlePoster, materialsMiniGroup, bigPoster);
 

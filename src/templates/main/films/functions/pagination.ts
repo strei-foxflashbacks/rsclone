8be6df8b89118d3a9a5e44ab4430f4f3dcfe015c
Inna T <meeoow005@gmail.com>
2023-02-26 23:53:01 +0100

    fix: path

--- src/templates/main/films/functions/pagination.ts
+++ src/templates/main/films/functions/pagination.ts
@@ -1,12 +1,12 @@
-import { mainPageController } from '../../../../components/controllers/filmPageController';
+import { mainPageController } from '../../../../components/controllers/filmController';
 
 const pagination = () => {
   const mainContent = <HTMLElement>document.querySelector('.films');
   if (!mainContent) throw new Error('mainContent don\'t find');
-  
+
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
   const mainContentHeight = mainContent.clientHeight;
-  
+
   if (scrollTop + clientHeight >= Math.min(scrollHeight, mainContentHeight) - 5) {
     mainPageController(mainContent);
   }
